b2ed79069f06d5378a3a9efd888db1d9220e051f
DanielTavares33 <danieltvs33@gmail.com>
2024-12-20 19:40:34 +0000

    Finalized the layout for the application

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import { Button } from "../components/ui/button.tsx"
-import { Input } from "../components/ui/input.tsx"
-import { Plus } from "lucide-react"
+import {Button} from "../components/ui/button.tsx"
+import {Input} from "../components/ui/input.tsx"
+import {Label} from "../components/ui/label.tsx"
+import {Plus, Trash} from "lucide-react"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "../components/ui/table.tsx"
 
 const Index = () => {
     return (
-        <div className="flex justify-center">
-            <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input />
-                <Button>
-                    <Plus />
-                </Button>
+        <div className="flex flex-col justify-center items-center space-y-8 mt-32">
+            <div className="gird w-full max-w-sm  items-center gap-1.5 space-y-2">
+                <Label htmlFor="todo">Add Todo</Label>
+                <div className="flex items-center space-x-2">
+                    <Input type="text" id="todo" />
+                    <Button>
+                        <Plus/>
+                    </Button>
+                </div>
+            </div>
+            <div className="flex w-full max-w-2xl justify-center">
+                <Table>
+                    <TableCaption>A list of todos.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Todo</TableHead>
+                            <TableHead className="text-right"></TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">INV001</TableCell>
+                            <TableCell className="text-right">
+                                <Button variant="destructive">
+                                    <Trash />
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </div>
     )
